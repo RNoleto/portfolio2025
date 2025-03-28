154a3893a316f6f150b4977fed19a13483995f3c
Ramon Noleto <devnoleto@gmail.com>
2025-03-28 14:50:09 -0300

    Teste quebra de linhas

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -9,8 +9,8 @@ import Polaroid from '../components/ui/Polaroid.vue'
         </div>
         <Polaroid imageSrc="../../public/img/RamonNoleto.jpg" caption="Ramon Noleto" class="absolute top-2 left-[205px] rotate-20 animeRight z-1"/>
         <div class="flex flex-col gap-2.5 text-body animeLeft mt-[-130px]">
-            <p>Sou um desenvolvedor web <br> especializado em criar <br> experiências digitais focadas <br> em performance e acessibilidade. </p>
-            <p>Minha missão é<br>transformar ideias em <br> soluções eficientes que <br> proporcionem aos usuários <br> uma navegação intuitiva e agradável.</p>
+            <p>Sou um desenvolvedor <br> web especializado em criar <br> experiências digitais <br> focadas em performance e <br> acessibilidade. </p>
+            <p>Minha missão é <br> transformar ideias em soluções <br> eficientes que <br> proporcionem aos usuários <br> uma navegação intuitiva e agradável.</p>
             <p>Ao longo da minha carreira, participei de diversos projetos desafiadores que me permitiram aprimorar habilidades em HTML, CSS, JavaScript, PHP, Vue. Cada experiência contribuiu para meu crescimento profissional e reforçou meu compromisso com a qualidade e a inovação.</p>
             <p>Estou sempre em busca de oportunidades para colaborar em projetos que valorizem a eficiência e a inclusão digital. Se você procura um profissional dedicado a entregar soluções web de alto desempenho e acessíveis a todos, vamos conversar!​</p>
         </div>

6e53dae8dc964d59f1d0edfd7792200342379bed
Ramon Noleto <devnoleto@gmail.com>
2025-03-28 15:07:49 -0300

    Teste quebra de linhas

--- src/components/ui/Polaroid.vue
+++ src/components/ui/Polaroid.vue
@@ -12,7 +12,7 @@ defineProps({
 </script>
 
 <template>
-  <div class="bg-white shadow-md rounded-lg p-4 pb-12 relative w-[200px] max-w-xs">
+  <div class="bg-white shadow-md rounded-lg p-4 pb-8 relative w-[200px] max-w-xs">
     <img :src="imageSrc" alt="Polaroid photo" class="w-full rounded-md" />
     <div class="absolute bottom-2 left-1/2 transform -translate-x-1/2 text-sm text-darkText font-detail">
       {{ caption }}

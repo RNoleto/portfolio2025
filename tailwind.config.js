51d7d9b0568b17c281cfabc22b890198a95be74c
Ramon Noleto <devnoleto@gmail.com>
2025-03-28 00:01:59 -0300

    Ajuste de thema tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,23 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#00bcff',
-        secondary: '#A6E3FF',
-      }
+        cream: '#FAF3E0',
+        lightCream: '#FFF8E1',
+        darkText: '#2C2C2C',
+        lightText: '#B0A99F',
+        cobaltBlue: '#0047AB',
+        turquoise: '#008CBA',
+        redCream: '#C0392B',
+        darkRedCream: '#800000',
+        secondary: '#B0A99F',
+      },
+      fontFamily: {
+        heading: ['Playfair Display', 'serif'],
+        body: ['Open Sans', 'sans-serif'],
+        code: ['Fira Code', 'monospace'],
+        codePro: ['Source Code Pro', 'monospace'],
+        anton: ['Anton', 'sans-serif'],
+      },
     },
   },
   plugins: [],

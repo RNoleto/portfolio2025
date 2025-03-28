321ecae20bfb9f041dccf117f8e716e7e22d6a13
Ramon Noleto <devnoleto@gmail.com>
2025-03-28 14:38:12 -0300

    :tada: Test: Usando o componente Polaroid na página AboutMe

--- src/components/ui/Polaroid.vue
+++ src/components/ui/Polaroid.vue
@@ -1,11 +1,25 @@
 <script setup>
+defineProps({
+  imageSrc: {
+    type: String,
+    required: true
+  },
+  caption: {
+    type: String,
+    default: ''
+  }
+});
 </script>
 
 <template>
-    <div>
-        <p>Componente para imitar uma foto polaroid para usar no AboutMe</p>
+  <div class="bg-white shadow-md rounded-lg p-4 pb-12 relative w-[200px] max-w-xs">
+    <img :src="imageSrc" alt="Polaroid photo" class="w-full rounded-md" />
+    <div class="absolute bottom-2 left-1/2 transform -translate-x-1/2 text-sm text-darkText font-detail">
+      {{ caption }}
     </div>
+  </div>
 </template>
 
 <style scoped>
+/* Os estilos estão definidos via Tailwind CSS */
 </style>
--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,15 +1,41 @@
 <script setup>
+import Polaroid from '../components/ui/Polaroid.vue'
 </script>
 
 <template>
-    <img src="/public/AboutMe.svg" alt="">
-    <section class="flex flex-col gap-2.5 text-body">
-        <p>Sou um desenvolvedor web especializado em criar experiências digitais focadas em performance e acessibilidade. </p>
-        <p>Minha missão é transformar ideias em soluções eficientes que proporcionem aos usuários uma navegação intuitiva e agradável.</p>
-        <p>Ao longo da minha carreira, participei de diversos projetos desafiadores que me permitiram aprimorar habilidades em HTML, CSS, JavaScript, PHP, Vue. Cada experiência contribuiu para meu crescimento profissional e reforçou meu compromisso com a qualidade e a inovação.</p>
-        <p>Estou sempre em busca de oportunidades para colaborar em projetos que valorizem a eficiência e a inclusão digital. Se você procura um profissional dedicado a entregar soluções web de alto desempenho e acessíveis a todos, vamos conversar!​</p>
+    <section class="aboutMe">
+        <div class="image-container relative">
+            <img src="/public/AboutMe.svg" alt="">
+        </div>
+        <Polaroid imageSrc="../../public/img/RamonNoleto.jpg" caption="Ramon Noleto" class="absolute top-2 left-[205px] rotate-20 animeRight z-1"/>
+        <div class="flex flex-col gap-2.5 text-body animeLeft mt-[-200px]">
+            <p class="w-[200px]">Sou um desenvolvedor web especializado em criar experiências digitais focadas em performance e acessibilidade. </p>
+            <p class="">Minha missão é<br>transformar ideias em <br> soluções eficientes que <br> proporcionem aos usuários <br> uma navegação intuitiva e agradável.</p>
+            <p>Ao longo da minha carreira, participei de diversos projetos desafiadores que me permitiram aprimorar habilidades em HTML, CSS, JavaScript, PHP, Vue. Cada experiência contribuiu para meu crescimento profissional e reforçou meu compromisso com a qualidade e a inovação.</p>
+            <p>Estou sempre em busca de oportunidades para colaborar em projetos que valorizem a eficiência e a inclusão digital. Se você procura um profissional dedicado a entregar soluções web de alto desempenho e acessíveis a todos, vamos conversar!​</p>
+        </div>
     </section>
 </template>
 
 <style scoped>
+.aboutMe {
+  position: relative;
+  padding: 1.25rem;
+  background: var(--color-cream);
+  height: calc(100vh - 64px);
+}
+
+.image-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: auto;
+}
+
+.image-container img {
+  width: 100%;
+  height: auto;
+  display: block;
+}
 </style>

08e3a440e10910b8a32d6050bf7fa89be99a955a
Ramon Noleto <devnoleto@gmail.com>
2025-03-29 15:44:43 -0300

    Teste de posicionamento dos stickers

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -6,7 +6,7 @@
     <section id="skills">
         <p>As principais tecnologias e ferramentas que utilizo diariamente para desenvolver soluções digitais eficientes, acessíveis e de alta performance.</p>
         <div class="image-container relative animeLeft mt-2.5">
-            <img src="/HabilidadesNovo.svg" alt="">
+            <img src="/Habilidade.png" alt="Habilidade">
         </div>
         <div class="mt-[105px] flex justify-between flex-wrap overflow-hidden">
             <!-- Html e Css -->
@@ -17,7 +17,7 @@
             <!-- JavaScript e Php -->
             <div class="flex">
                 <img src="/img/stickers/javascript.svg" alt="javacript">
-                <img src="/img/stickers/php.svg" alt="php" class="ml-[-20px] mr-[30px]">
+                <img src="/img/stickers/php.svg" alt="php" class="ml-[-20px] mr-[20px]">
             </div>
             <div class="flex">
                 <img src="/img/stickers/vue.svg" alt="vue">
@@ -26,7 +26,7 @@
 
             <div class="flex">
                 <img src="/img/stickers/wordpress.svg" alt="wordpress">
-                <img src="/img/stickers/figma.svg" alt="figma" class="ml-[-35px] mr-[40px]">
+                <img src="/img/stickers/figma.svg" alt="figma" class="ml-[-35px] mr-[20px]">
             </div>
 
             <div class="flex">

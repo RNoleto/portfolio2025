d05d7528140f4b755e01b7966fc9d5efc82d8fa9
Ramon Noleto <devnoleto@gmail.com>
2025-03-29 13:56:09 -0300

    Ajuste de posicionamento do Hero e Links do menu

--- src/views/Hero.vue
+++ src/views/Hero.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-    <section class="overflow-hidde flex flex-col" id="home">
+    <section class="overflow-hidde flex flex-col mt-[64px]" id="home">
         <div class="animeLeft">
-            <p class="title mt-[30px]">Olá! Eu sou Ramon.</p>
+            <p class="title">Olá! Eu sou Ramon.</p>
             <p class="featuredText mt-1">Desenvolvendo experiências digitais com foco em <span class="detail">performance</span> e <span class="detail">acessibilidade.</span></p>
             <p class="font-detail mt-5 text-end text-lg">Transformando ideias em soluções digitais</p>
         </div>

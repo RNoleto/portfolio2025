3efd004877d5e03b62c85abb3fa36a331e11c528
Ramon Noleto <devnoleto@gmail.com>
2025-03-29 17:13:25 -0300

    Ajuste dos Stickers e do titulo Habilidades

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -5,33 +5,35 @@
 <template>
     <section id="skills">
         <p>As principais tecnologias e ferramentas que utilizo diariamente para desenvolver soluções digitais eficientes, acessíveis e de alta performance.</p>
-        <div class="image-container relative animeLeft mt-2.5">
-            <img src="/Habilidade.png" alt="Habilidade">
+        <!-- Barra de Habilidade -->
+        <div class="image-container relative animeLeft mt-2.5 bg-turquoise shadow-sm">
+            <img src="/Habilidades.svg" alt="Habilidade" class="m-auto">
         </div>
-        <div class="mt-[105px] flex justify-between flex-wrap overflow-hidden">
+        <div class="mt-[120px] flex justify-between flex-wrap overflow-hidden">
             <!-- Html e Css -->
             <div class="flex">
-                <img src="/img/stickers/html.svg" alt="html">
-                <img src="/img/stickers/css.svg" alt="css" class="ml-[-30px]">
+                <img src="/img/stickers/html.png" alt="html" class="sticker h-[120px]">
+                <img src="/img/stickers/css.png" alt="css" class="sticker h-[90px] ml-[-40px]">
             </div>
             <!-- JavaScript e Php -->
             <div class="flex">
-                <img src="/img/stickers/javascript.svg" alt="javacript">
-                <img src="/img/stickers/php.svg" alt="php" class="ml-[-20px] mr-[20px]">
+                <img src="/img/stickers/javascript.png" alt="javacript" class="sticker h-[90px]">
+                <img src="/img/stickers/php.png" alt="php" class="sticker h-[70px] ml-[-20px]">
             </div>
+            <!-- Vue e React -->
             <div class="flex">
-                <img src="/img/stickers/vue.svg" alt="vue">
-                <img src="/img/stickers/react.svg" alt="react" class="ml-[-25px]">
+                <img src="/img/stickers/vue.png" alt="vue" class="sticker h-[120px]">
+                <img src="/img/stickers/react.png" alt="react" class="sticker h-[100px] ml-[-30px]">
             </div>
-
+            <!-- Wordpress e Figma -->
             <div class="flex">
-                <img src="/img/stickers/wordpress.svg" alt="wordpress">
-                <img src="/img/stickers/figma.svg" alt="figma" class="ml-[-35px] mr-[20px]">
+                <img src="/img/stickers/wordpress.png" alt="wordpress" class="sticker h-[80px]">
+                <img src="/img/stickers/figma.png" alt="figma" class="sticker h-[90px] ml-[-35px]">
             </div>
-
+            <!-- MySQL e PostgreSQL -->
             <div class="flex">
-                <img src="/img/stickers/mysql.svg" alt="mysql">
-                <img src="/img/stickers/postgresql2.png" alt="" class="postgresql ml-[-55px]">
+                <img src="/img/stickers/mysql.png" alt="mysql" class="sticker h-[100px]">
+                <img src="/img/stickers/postgresql.png" alt="" class="sticker h-[100px] ml-[-55px]">
             </div>
         </div>
     </section>
@@ -52,8 +54,7 @@
     width: 100%;
 }
 
-.postgresql{
+.sticker{
     width: auto;
-    height: 120px;
 }
 </style>

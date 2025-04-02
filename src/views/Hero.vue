5bdd697dbe156c23500a14f4686514887d6d03ca
Ramon Noleto <devnoleto@gmail.com>
2025-04-02 12:32:58 -0300

    :tada: centralização vertical conteudo hero

--- src/views/Hero.vue
+++ src/views/Hero.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <section class="overflow-hidde flex flex-col mt-[64px]" id="home">
+    <section class="overflow-hidde flex flex-col mt-[64px] justify-center" id="home">
         <div class="animeLeft">
             <p class="title">Olá! Eu sou Ramon.</p>
             <p class="featuredText mt-1">Desenvolvendo experiências digitais com foco em <span class="detail">performance</span> e <span class="detail">acessibilidade.</span></p>

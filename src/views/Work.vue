a98297d28327752f3fb79917562dd7962046e3c8
Ramon Noleto <devnoleto@gmail.com>
2025-04-02 16:20:12 -0300

    :tada: feat: Criação da página de experiencias de trabalho

---
+++ src/views/Work.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+const work = [
+    {
+        name: '',
+        position: '',
+        firm: '',
+        period: '',
+        description: ''
+    }
+]
+</script>
+
+<template>
+    <section>
+        
+    </section>
+</template>
+
+<style scoped></style>

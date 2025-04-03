c2f332851d269e273fb6d44c14da765de877d172
Ramon Noleto <devnoleto@gmail.com>
2025-04-03 10:40:20 -0300

    Ajuste de subtitulo no Hero para usar fonte padrão, ajuste de tamanho para 16px

--- src/views/Hero.vue
+++ src/views/Hero.vue
@@ -7,7 +7,7 @@
         <div class="animeLeft">
             <p class="title">Olá! Eu sou Ramon.</p>
             <p class="featuredText mt-1">Desenvolvendo experiências digitais com foco em <span class="detail">performance</span> e <span class="detail">acessibilidade.</span></p>
-            <p class="font-detail mt-5 text-end text-lg">Transformando ideias em soluções digitais</p>
+            <p class="mt-5 text-end text-md">Transformando ideias em soluções digitais</p>
         </div>
     </section>
 </template>

4f2454e190e47b2e97d1e2b174e0022c2a9f9845
Ramon Noleto <devnoleto@gmail.com>
2025-04-03 23:25:33 -0300

    Ajuste de responsividade para telas maiores

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -7,10 +7,10 @@ import Polaroid from '../components/ui/Polaroid.vue'
         <div class="image-container relative animeLeft">
             <img src="/AboutMe.svg" alt="">
         </div>
-        <Polaroid imageSrc="../img/RamonNoleto.jpg" caption="Ramon Noleto" class="absolute top-2 left-[205px] rotate-20 animeRight z-1 sm:left-[450px] sm:w-[250px] lg:left-[700px]"/>
-        <div class="flex flex-col gap-2.5 text-body animeLeft mt-[-180px] sm:mt-[-100px] lg:mt-[-60px]">
-            <p>Sou um desenvolvedor <br class="sm:hidden"> web especializado em <br class="lg:hidden"> criar <br class="sm:hidden"> experiências <br class="sm:hidden lg:block"> digitais <br class="sm:hidden"> focadas em <br class="hidden sm:block lg:hidden"> performance <br class="sm:hidden"> e <br class="hidden"> acessibilidade.</p>
-            <p>Minha missão é <br class="sm:hidden"> transformar ideias em <br class="sm:hidden">soluções eficientes <br class="hidden sm:block lg:hidden">que <br class="sm:hidden"> proporcionem aos usuários <br class="sm:hidden lg:block"> uma navegação intuitiva e <br class="hidden"> agradável.</p>
+        <Polaroid imageSrc="../img/RamonNoleto.jpg" caption="Ramon Noleto" class="absolute top-2 left-[205px] rotate-20 animeRight z-1 sm:left-[450px] sm:w-[250px] lg:left-[700px] xl:left-[870px]"/>
+        <div class="flex flex-col gap-2.5 text-body animeLeft mt-[-180px] sm:mt-[-100px] lg:mt-[-60px] xl:mt-[-20px]">
+            <p>Sou um desenvolvedor <br class="sm:hidden"> web especializado em <br class="lg:hidden"> criar <br class="sm:hidden"> experiências <br class="sm:hidden lg:block xl:hidden"> digitais <br class="sm:hidden"> focadas em <br class="hidden sm:block lg:hidden"> performance <br class="sm:hidden"> e <br class="hidden xl:block"> acessibilidade.</p>
+            <p>Minha missão é <br class="sm:hidden"> transformar ideias em <br class="sm:hidden">soluções eficientes <br class="hidden sm:block lg:hidden">que <br class="sm:hidden"> proporcionem aos usuários <br class="sm:hidden lg:block xl:hidden"> uma navegação intuitiva e <br class="hidden"> agradável.</p>
             <p>Ao longo da minha carreira, participei de diversos projetos desafiadores que me permitiram aprimorar habilidades <br class="hidden"> em HTML, CSS, JavaScript, PHP, Vue. Cada experiência contribuiu para meu crescimento profissional e reforçou meu compromisso com a qualidade e a inovação.</p>
             <p>Estou sempre em busca de oportunidades para colaborar em projetos que valorizem a eficiência e a inclusão digital. Se você procura um profissional dedicado a entregar soluções web de alto desempenho e acessíveis a todos, vamos conversar!​</p>
         </div>

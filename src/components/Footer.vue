fe275a0fc3f35a1b292f72b10e49c6f6b371ff76
Ramon Noleto <devnoleto@gmail.com>
2025-04-04 15:00:20 -0300

    Teste de contraste de cores do footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <footer class="py-6 px-4 text-center bg-turquoise text-cream text-sm lg:text-xl">
+    <footer class="py-6 px-4 text-center bg-darkBlue text-lightCream text-sm lg:text-xl">
         <p>Alguns direitos reservados.</p>
         <p>Desenvolvido por Ramon Noleto</p>
         <p>devnoleto@gmail.com</p>
